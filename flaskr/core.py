9abee881e114ccd21a18f8d291373cdc421fee1a
Alex Yu <alexyu99126@gmail.com>
2019-03-10 12:15:23 -0700

    Added upload form, added run.py

--- flaskr/core.py
+++ flaskr/core.py
@@ -1,12 +1,22 @@
-import functools
+import functools, os, sys
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+import numpy as np
 
+DATA_DIR = 'data'
+UPLOAD_FOLDER = 'uploads'
 bp = Blueprint('core', __name__, url_prefix='/')
+
+# load data
+keys = np.load(os.path.join(DATA_DIR, 'keys.npy'))
 
 @bp.route('/', methods=('GET', 'POST'))
 def home():
     return render_template('core/home.html')
+
+@bp.route('/query', methods=('POST',))
+def query_image():
+    
+    return ''
