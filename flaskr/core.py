2b94bf18061aa00d6c63b7ce02c6637175dfdda9
Alex Yu <alexyu99126@gmail.com>
2019-03-12 15:40:03 +0000

    added checks for file type/size in JS and Python, added route public/, fixed some grammar and style.

--- flaskr/core.py
+++ flaskr/core.py
@@ -10,12 +10,15 @@ from flask import (
 from werkzeug.utils import secure_filename
 
 DATA_FOLDER = 'data'
+PUBLIC_FOLDER = '../public'
 QUERY_FOLDER = '/datax/query'
 TMP_FOLDER = 'tmp'
 SYS_TMP_FOLDER = tempfile.gettempdir()
 
 STATIC_FOLDER = os.path.join('flaskr', 'static')
-ALLOWED_EXTENSIONS = set(['npy', 'npz'])
+
+ALLOWED_EXTENSIONS = set(['npy'])
+MAX_FILE_SIZE = 32 * 1024 * 1024
 
 KEYS_PATH = os.path.join(QUERY_FOLDER, 'keys.npy')
 META_PATH = os.path.join(QUERY_FOLDER, 'meta.npy')
@@ -86,7 +89,13 @@ def home():
     
 @bp.route('/image/<path:filename>', methods=('GET',))
 def serve_image(filename):
+    if filename[:7] != 'bl-img-' or filename[-4:] != '.png':
+        abort(403)
     return send_from_directory(SYS_TMP_FOLDER, filename)
+
+@bp.route('/public/<path:filename>', methods=('GET',))
+def serve_public(filename):
+    return send_from_directory(PUBLIC_FOLDER, filename)
 
 @bp.route('/query', methods=('POST',))
 def query_image():
@@ -97,57 +106,63 @@ def query_image():
         if file.filename == '':
             return jsonify({'message': 'No file specified'})
 
+        file.seek(0, os.SEEK_END)
+        file_length = file.tell()
+        if file_length > MAX_FILE_SIZE:
+            return jsonify({'message': 'File exceeds size limit of ' + str(MAX_FILE_SIZE//1024//1024) + ' MB'})
+        file.seek(0, os.SEEK_SET)
         if file and allowed_file(file.filename):
-            # save the file
-            filename = os.path.join(TMP_FOLDER, 'f' + next(tempfile._get_candidate_names()))
-            file.save(filename)
+            try:
+                # save the file
+                filename = os.path.join(TMP_FOLDER, 'f' + next(tempfile._get_candidate_names()))
+                file.save(filename)
 
-            #try:
-            im = np.load(filename).astype(np.float32)
-            im *= (1.0/im.max())
+                #try:
+                im = np.load(filename).astype(np.float32)
+                im *= (1.0/im.max())
 
-            # check image size
-            if im.shape != (16, 128):
-                print("Unsupported image dimensions received")
-                return jsonify({'message': 'unsupported image dimensions. Only 16x128 images allowed.'})
-            im = im.reshape((1, 16, 128, 1)).astype(np.float32)
+                # check image size
+                if im.shape != (16, 128):
+                    print("Unsupported image dimensions received")
+                    return jsonify({'message': 'unsupported image dimensions. Only 16x128 images allowed.'})
+                im = im.reshape((1, 16, 128, 1)).astype(np.float32)
 
-            # run tensorflow inference
-            vec = sess.run(y, feed_dict={x: im})
-            
-            # compare with keys
-            sim = keys @ vec.reshape((32,))
-            idx = np.argmax(sim)
-            
-            im_ = _retrieve_image(meta[idx])
-            if im_ is None:
-                im_ = np.ones_like(im)
-            wat_path = 'i' + next(tempfile._get_candidate_names()) + '.png'
-            im_ *= (1.0/im_.max())
+                # run tensorflow inference
+                vec = sess.run(y, feed_dict={x: im})
+                
+                # compare with keys
+                sim = keys @ vec.reshape((32,))
+                idx = np.argmax(sim)
+                
+                im_ = _retrieve_image(meta[idx])
+                if im_ is None:
+                    im_ = np.ones_like(im)
+                wat_path = 'bl-img-' + next(tempfile._get_candidate_names()) + '.png'
+                im_ *= (1.0/im_.max())
 
-            _plot_image(os.path.join(SYS_TMP_FOLDER, wat_path), im.squeeze(), im_.squeeze())
+                _plot_image(os.path.join(SYS_TMP_FOLDER, wat_path), im.squeeze(), im_.squeeze())
 
-            #out_im = Image.fromarray(np.ones((100,100), dtype=np.uint8) * idx * 2)
-            #out_im.save(os.path.join(TMP_FOLDER, wat_path))
+                prefix, time_stamp, obstarget, scan_num, coarse_chan, subidx = _parse_meta(meta[idx])
 
-
-            prefix, time_stamp, obstarget, scan_num, coarse_chan, subidx = _parse_meta(meta[idx])
-
-            f0 = 2802.83203125 #Sband
-            foff =-2.7939677238464355e-6
-            centerfreq = f0 + foff*(float(coarse_chan)*1024**2+float(subidx))
-            obj = {
-                'prefix': prefix,
-                'target': obstarget,
-                'timestamp': time_stamp,
-                'scannum': scan_num,
-                "band": "S", 
-                'coarsechnl': coarse_chan,
-                'centerfreq': float(centerfreq), 
-                'idx': int(idx),
-                'dist': float(1.-sim[idx]),
-                'wat_path': '/image/' + wat_path
-            }
-            
-            os.remove(filename)
-            return jsonify(obj)
+                f0 = 2802.83203125 #Sband
+                foff =-2.7939677238464355e-6
+                centerfreq = f0 + foff*(float(coarse_chan)*1024**2+float(subidx))
+                obj = {
+                    'prefix': prefix,
+                    'target': obstarget,
+                    'timestamp': time_stamp,
+                    'scannum': scan_num,
+                    "band": "S", 
+                    'coarsechnl': coarse_chan,
+                    'centerfreq': float(centerfreq), 
+                    'idx': int(idx),
+                    'dist': float(1.-sim[idx]),
+                    'wat_path': '/image/' + wat_path
+                }
+                
+                os.remove(filename)
+                return jsonify(obj)
+            except:
+                return jsonify({'message': 'Unknown error'})
+        else:
+            return jsonify({'message': 'Unsupported format, only .npy allowed'})
--- flaskr/static/form.js
+++ flaskr/static/form.js
@@ -1,46 +1,73 @@
 (function(){
     $(document).ready(function () {
+        const MAX_FILE_SIZE = 32 * 1024 * 1024; // max acceptable file size in bytes
+        const SUPPORTED_FORMATS = ['npy']; // supported file types
+
+        var show_error = function(msg) {
+            $('#results').hide();
+            $('#input-error').text("Error: " + msg);
+        };
+
         $("#image-query-form").submit(function(e) {
             e.preventDefault(); // avoid to execute the actual submit of the form.
 
             var form = $(this);
             var url = form.attr('action');
             var data = new FormData(this);
+            if ($("#image-query-form")[0][0].files.length == 0) {
+                show_error('No file specified');
+                return;
+            }
+            var good = true;
             jQuery.each($("#image-query-form")[0][0].files, function(i, file) {
+                // input pre-validation
+                if (file.size > MAX_FILE_SIZE) {
+                    show_error('Maximum file size exceeded');
+                    good = false;
+                    return;
+                }
+                ext = file.name.substr(file.name.lastIndexOf('.') + 1);
+                if (SUPPORTED_FORMATS.indexOf(ext) < 0) {
+                    show_error('Unsupported format, only .npy allowed');
+                    good = false;
+                    return;
+                }
                 data.append('file', file);
             });
+            if (!good) {
+                return;
+            }
 
             $.ajax({
-               type: "POST",
-               url: url,
-               data: data,
-               enctype: 'multipart/form-data',
-               processData: false,
-               contentType: false,
-               cache: false,
-               success: function(data)
-               {
-		   if('idx' in data) {
-                     $('#disp-dist').text(data['dist']);
-                     $('#disp-timestamp').text(data['timestamp']);
-                     $('#disp-target').text(data['target']);
-                     $('#disp-scannum').text(data['scannum']);
-                     $('#disp-band').text(data['band']);
-                     $('#disp-coarsechnl').text(data['coarsechnl']);
-                     $('#disp-prefix').text(data['prefix']);
-                     $('#disp-id').text(data['idx']);
-                     $('#disp-centerfreq').text(data['centerfreq']);
-                     $('#disp-img').html("<img src='" + data['wat_path'] + "' alt='waterfall'/>");
-                     $('#input-error').text("");
-                     $('#results').show();
-		   } else {
-                     $('#results').hide();
-                     $('#input-error').text("Error: " + data['message']);
-		   }
-               },
-               error: function(data) {
-	       }
-             });
+                type: "POST",
+                url: url,
+                data: data,
+                enctype: 'multipart/form-data',
+                processData: false,
+                contentType: false,
+                cache: false,
+                success: function(data)
+                {
+                    if('idx' in data) {
+                        $('#disp-dist').text(data['dist']);
+                        $('#disp-timestamp').text(data['timestamp']);
+                        $('#disp-target').text(data['target']);
+                        $('#disp-scannum').text(data['scannum']);
+                        $('#disp-band').text(data['band']);
+                        $('#disp-coarsechnl').text(data['coarsechnl']);
+                        $('#disp-prefix').text(data['prefix']);
+                        $('#disp-id').text(data['idx']);
+                        $('#disp-centerfreq').text(data['centerfreq']);
+                        $('#disp-img').html("<img src='" + data['wat_path'] + "' alt='waterfall'/>");
+                        $('#input-error').text("");
+                        $('#results').show();
+                    } else {
+                        show_error(data['message']);
+                    }
+                },
+                error: function(data) {
+                }
+            });
         });
     });
 })()
