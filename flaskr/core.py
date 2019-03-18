0a5cee467c6db740f620d7a0268fba5b138e5316
Alex Yu <alexyu99126@gmail.com>
2019-03-18 19:42:55 +0000

    fixed performance issue caused by heapq/argsort by using quickselect algorithm

--- flaskr/core.py
+++ flaskr/core.py
@@ -5,8 +5,8 @@ import tempfile
 from PIL import Image
 import matplotlib.pyplot as plt
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory, jsonify, abort
-)
+        Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory, jsonify, abort
+        )
 from werkzeug.utils import secure_filename
 
 DATA_FOLDER = 'data'
@@ -20,27 +20,39 @@ STATIC_FOLDER = os.path.join('flaskr', 'static')
 ALLOWED_EXTENSIONS = set(['npy'])
 MAX_FILE_SIZE = 32 * 1024 * 1024
 
-KEYS_PATH = os.path.join(QUERY_FOLDER, 'keys.npy')
+MODEL_NAMES = [ "Conv Noise", "Fully Connected" ]
+MODEL_PATHS = [
+        os.path.join(DATA_FOLDER, 'conv_noise.pb'),
+        os.path.join(DATA_FOLDER, 'frozen_model.pb')
+        ]
+
+KEY_PATHS = [
+        os.path.join(QUERY_FOLDER, 'keys_conv_noise.npy'),
+        os.path.join(QUERY_FOLDER, 'keys.npy')
+        ]
+
+
 META_PATH = os.path.join(QUERY_FOLDER, 'meta.npy')
-MODEL_PATH = os.path.join(DATA_FOLDER, 'frozen_model.pb')
 
 bp = Blueprint('core', __name__, url_prefix='/')
 
 # load data
-keys = np.load(KEYS_PATH)
+keys = [np.load(path) for path in KEY_PATHS]
 meta = np.load(META_PATH)
 
 def _load_graph(frozen_graph_filename):
-   """ Function to load frozen TensorFlow graph"""
-   with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-       graph_def = tf.GraphDef()
-       graph_def.ParseFromString(f.read())
-   with tf.Graph().as_default() as graph:
-       tf.import_graph_def(graph_def, name="prefix")
-   return graph
+    """ Function to load frozen TensorFlow graph"""
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def, name="prefix")
+    return graph
 
-def _plot_image(target_path, img, img_):
-    f, ax = plt.subplots(2, 1, figsize=(8,5))
+def _plot_image(target_path, img, img_, f=None, ax=None):
+    if f is None:
+        f, ax = plt.subplots(2, 1, figsize=(8,5))
+    ax[0].cla(); ax[1].cla()
     ax[0].imshow(img.squeeze(), aspect='auto')
     ax[0].set_title("Your Input")
     ax[1].imshow(img_.squeeze(), aspect='auto')
@@ -48,7 +60,7 @@ def _plot_image(target_path, img, img_):
     ax[1].set_ylabel("Time")
     plt.tight_layout()
     plt.savefig(target_path)
-    return 
+    return f, ax
 
 def _parse_meta(meta):
     basename = '_'.join(meta[0].split('.'))
@@ -71,22 +83,20 @@ def _retrieve_image(meta, data_dir="/datax/theta3_split/"):
         img_ = None
     return img_
 
-graph = _load_graph(MODEL_PATH)
-x = graph.get_tensor_by_name('prefix/input_:0')
-y = graph.get_tensor_by_name('prefix/enc_norm:0')
-sess = tf.Session(graph=graph)
-
-num_dims = keys.shape[1]
+graphs = [_load_graph(path) for path in MODEL_PATHS]
+x = [g.get_tensor_by_name('prefix/input_:0') for g in graphs]
+y = [g.get_tensor_by_name('prefix/enc_norm:0') for g in graphs]
+sess = [tf.Session(graph=g) for g in graphs]
 
 def allowed_file(filename):
     # returns true if file's extension is supported
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route('/', methods=('GET', 'POST'))
 def home():
     return render_template('core/home.html', wat_image=request.args.get('wat_image'))
-    
+
 @bp.route('/image/<path:filename>', methods=('GET',))
 def serve_image(filename):
     if filename[:7] != 'bl-img-' or filename[-4:] != '.png':
@@ -100,9 +110,15 @@ def serve_public(filename):
 @bp.route('/query', methods=('POST',))
 def query_image():
     if request.method == 'POST':
+        if 'num-results' not in request.form:
+            return jsonify({'message': 'Number of results required'})
+        if 'model-id' not in request.form:
+            return jsonify({'message': 'Model ID required'})
         if 'file' not in request.files:
             return jsonify({'message': 'No file specified'})
         file = request.files['file']
+        num_results = int(request.form.get('num-results'))
+        model_id = int(request.form.get('model-id'))
         if file.filename == '':
             return jsonify({'message': 'No file specified'})
 
@@ -117,7 +133,6 @@ def query_image():
                 filename = os.path.join(TMP_FOLDER, 'f' + next(tempfile._get_candidate_names()))
                 file.save(filename)
 
-                #try:
                 im = np.load(filename).astype(np.float32)
                 im *= (1.0/im.max())
 
@@ -128,40 +143,53 @@ def query_image():
                 im = im.reshape((1, 16, 128, 1)).astype(np.float32)
 
                 # run tensorflow inference
-                vec = sess.run(y, feed_dict={x: im})
-                
+                vec = sess[model_id].run(y[model_id], feed_dict={x[model_id]: im})
+
                 # compare with keys
-                sim = keys @ vec.reshape((32,))
-                idx = np.argmax(sim)
-                
-                im_ = _retrieve_image(meta[idx])
-                if im_ is None:
-                    im_ = np.ones_like(im)
-                wat_path = 'bl-img-' + next(tempfile._get_candidate_names()) + '.png'
-                im_ *= (1.0/im_.max())
+                sim = keys[model_id] @ vec.reshape((32,))
 
-                _plot_image(os.path.join(SYS_TMP_FOLDER, wat_path), im.squeeze(), im_.squeeze())
+                # find indices of best matches
+                idxs = []
+                if num_results == 1:
+                    # fast-forward if only one match required
+                    idxs = [np.argmax(sim)]
+                else:
+                    # else use quickselect partition
+                    idxs = np.argpartition(-sim, np.arange(num_results))[:num_results]
 
-                prefix, time_stamp, obstarget, scan_num, coarse_chan, subidx = _parse_meta(meta[idx])
+                # create result set
+                objs = []
+                f_, ax_ = None, None
+                for idx in idxs:
+                    im_ = _retrieve_image(meta[idx])
+                    if im_ is None:
+                        im_ = np.ones_like(im)
+                    wat_path = 'bl-img-' + next(tempfile._get_candidate_names()) + '.png'
+                    im_ *= (1.0/im_.max())
 
-                f0 = 2802.83203125 #Sband
-                foff =-2.7939677238464355e-6
-                centerfreq = f0 + foff*(float(coarse_chan)*1024**2+float(subidx))
-                obj = {
-                    'prefix': prefix,
-                    'target': obstarget,
-                    'timestamp': time_stamp,
-                    'scannum': scan_num,
-                    "band": "S", 
-                    'coarsechnl': coarse_chan,
-                    'centerfreq': float(centerfreq), 
-                    'idx': int(idx),
-                    'dist': float(1.-sim[idx]),
-                    'wat_path': '/image/' + wat_path
-                }
-                
+                    f_, ax_ = _plot_image(os.path.join(SYS_TMP_FOLDER, wat_path), im.squeeze(), im_.squeeze(), f_, ax_)
+
+                    prefix, time_stamp, obstarget, scan_num, coarse_chan, subidx = _parse_meta(meta[idx])
+
+                    f0 = 2802.83203125 #Sband
+                    foff =-2.7939677238464355e-6
+                    centerfreq = f0 + foff*(float(coarse_chan)*1024**2+float(subidx))
+                    dist = (2. * max(0., 1.-sim[idx])) ** 0.5
+                    objs.append({
+                        'prefix': prefix,
+                        'target': obstarget,
+                        'timestamp': time_stamp,
+                        'scannum': scan_num,
+                        "band": "S", 
+                        'coarsechnl': coarse_chan,
+                        'centerfreq': float(centerfreq), 
+                        'idx': int(idx),
+                        'dist': float(dist),
+                        'wat_path': '/image/' + wat_path
+                        });
+
                 os.remove(filename)
-                return jsonify(obj)
+                return jsonify({'message':'success', 'results': objs})
             except:
                 return jsonify({'message': 'Unknown error'})
         else:
--- flaskr/static/form.js
+++ flaskr/static/form.js
@@ -3,71 +3,110 @@
         const MAX_FILE_SIZE = 32 * 1024 * 1024; // max acceptable file size in bytes
         const SUPPORTED_FORMATS = ['npy']; // supported file types
 
-        var show_error = function(msg) {
+        var showError = function(msg) {
             $('#results').hide();
+            $('#input-error').css('color', '#c22');
             $('#input-error').text("Error: " + msg);
+            $('#input-error').show();
         };
+
+        var showInfo = function(msg) {
+            $('#input-error').css('color', '#222');
+            $('#input-error').text(msg);
+            $('#input-error').show();
+        };
+
 
         $("#image-query-form").submit(function(e) {
             e.preventDefault(); // avoid to execute the actual submit of the form.
 
             var form = $(this);
             var url = form.attr('action');
-            var data = new FormData(this);
+            var fdata = new FormData(this);
             if ($("#image-query-form")[0][0].files.length == 0) {
-                show_error('No file specified');
+                showError('No file specified');
                 return;
             }
             var good = true;
             jQuery.each($("#image-query-form")[0][0].files, function(i, file) {
                 // input pre-validation
                 if (file.size > MAX_FILE_SIZE) {
-                    show_error('Maximum file size exceeded');
+                    showError('Maximum file size exceeded');
                     good = false;
                     return;
                 }
                 ext = file.name.substr(file.name.lastIndexOf('.') + 1);
                 if (SUPPORTED_FORMATS.indexOf(ext) < 0) {
-                    show_error('Unsupported format, only .npy allowed');
+                    showError('Unsupported format, only .npy allowed');
                     good = false;
                     return;
                 }
-                data.append('file', file);
+                fdata.append('file', file);
             });
             if (!good) {
                 return;
             }
+            fdata.append('num-results', parseInt($('#num-results')[0].value));
+            fdata.append('model-id', parseInt($('#model-id')[0].value));
+            showInfo("Querying, please wait...");
 
+            var showSingleResult = function(idx) {
+                var data = results[idx];
+                $('#disp-dist').text(data['dist']);
+                $('#disp-timestamp').text(data['timestamp']);
+                $('#disp-target').text(data['target']);
+                $('#disp-scannum').text(data['scannum']);
+                $('#disp-band').text(data['band']);
+                $('#disp-coarsechnl').text(data['coarsechnl']);
+                $('#disp-prefix').text(data['prefix']);
+                $('#disp-id').text(data['idx']);
+                $('#disp-centerfreq').text(data['centerfreq']);
+                $('#disp-img').html("<img src='" + data['wat_path'] + "' alt='waterfall'/>");
+                $('#single-result').show();
+                for (var i = 0; i < results.length; i++) {
+                    if (i == idx) $('#result-entry-' + i).addClass('result-entry-active');
+                    else $('#result-entry-' + i).removeClass('result-entry-active');
+                }
+            }
             $.ajax({
                 type: "POST",
                 url: url,
-                data: data,
-                enctype: 'multipart/form-data',
+                data: fdata,
+                //enctype: 'multipart/form-data',
                 processData: false,
                 contentType: false,
                 cache: false,
                 success: function(data)
                 {
-                    if('idx' in data) {
-                        $('#disp-dist').text(data['dist']);
-                        $('#disp-timestamp').text(data['timestamp']);
-                        $('#disp-target').text(data['target']);
-                        $('#disp-scannum').text(data['scannum']);
-                        $('#disp-band').text(data['band']);
-                        $('#disp-coarsechnl').text(data['coarsechnl']);
-                        $('#disp-prefix').text(data['prefix']);
-                        $('#disp-id').text(data['idx']);
-                        $('#disp-centerfreq').text(data['centerfreq']);
-                        $('#disp-img').html("<img src='" + data['wat_path'] + "' alt='waterfall'/>");
-                        $('#input-error').text("");
+                    if('results' in data) {
+                        results = data['results']
+                        var resultsHtml = "";
+                        for (var i = 0; i < results.length; i++) {
+                            resultsHtml += "<div class=\"col-sm-2 result-entry\" id=\"result-entry-" + i + "\"><strong>" +
+                                (i+1) + "</strong><br/>d = " + (Math.round(results[i]['dist'] * 1e7)/1e7) + "</div>";
+                        }
+                        var resultsList = $('#results-list');
+                        console.log(results);
+                        resultsList.html(resultsHtml);
+                        for (var i = 0; i < results.length; i++) {
+                            $('#result-entry-' + i).on('click', function(event) {
+                                var i = parseInt(this.id.substr(13));
+                                showSingleResult(i);
+                            });
+                        }
+
                         $('#results').show();
+                        $('#input-error').hide();
+
+                        // show first result by default
+                        showSingleResult(0);
                     } else {
-                        show_error(data['message']);
+                        showError(data['message']);
                     }
                 },
                 error: function(data) {
                 }
-            });
+            }, 'json');
         });
     });
 })()
